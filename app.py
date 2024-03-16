9bc4c02c42a3b9436381918486cc2feeacd67ed7
Tim Quattrochi <timq82@gmail.com>
2024-03-16 17:36:25 -0400

    Refactor code to use a function for asking questions

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
+
+
 questions = ["What is your favorite color? ",
              "What is your favorite food? ", "What is your favorite animal? "]
 
 answers = {}
 
+
+def ask_questions(questions):
+    global answers  # I am using the global keyword because I intend to modify the global variable answers
+    for question in questions:
+        answer = input(question)
+        if len(answer) == 0:
+            continue
+        answers[question] = answer
+
+
 # Gets the last word of the question, replaces the question mark with an empty string
-
-
 def last_word(sentence):
     return sentence.split()[-1].replace('?', '')
 
 
-for question in questions:
-    answer = input(question)
-    if len(answer) == 0:
-        continue
-    answers[question] = answer
+ask_questions(questions)
 
 for question, answer in answers.items():
     print(f"Your favorite {last_word(question)} is {answer}")
